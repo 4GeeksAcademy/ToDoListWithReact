64dfef66b559b2edf0f1c0bb5e21e13711e5c5f2
TomasVentura17 <126104459+TomasVentura17@users.noreply.github.com>
2023-05-15 17:03:10 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [input, setInput] = useState("");
+	const [arrTodo, setArrTodo] = useState([]);
+	const [deleteShown, setDeleteShown] = useState(false);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="mb-3 w-75 mx-auto">
+				<label htmlFor="exampleInputEmail1" className="form-label">
+					<h2 className="h2">Todos</h2>
+				</label>
+				<div >
+				<form  onSubmit = {evento => {
+					evento.preventDefault();
+					if (input.length > 0)
+						setArrTodo([...arrTodo, input]);
+					setInput("");
+				}}>
+            
+          <input
+						type="text"
+						className="form-control"
+						value={input}
+						placeholder="What needs to be done?"
+						onChange={(e) => {
+							setInput(e.target.value);
+						}}
+					/>
+        </form>
+				</div>
+				{arrTodo.map((e, i) => {
+					return (
+						<div
+							className="form-control"
+							key={i}
+							onMouseEnter={() => setDeleteShown(i)}
+							onMouseLeave={() => setDeleteShown(null)}>
+							{e}{" "}
+							{deleteShown == i && (
+								<button
+									type="button"
+									className="btn-close btn-close-danger"
+									aria-label="Close"
+									onClick={() => {
+										setArrTodo(
+											arrTodo.filter(
+												(v, index) => index !== i
+											)
+										);
+									}}></button>
+							)}
+						</div>
+					);
+				})}
+				<div>
+					<footer>
+						{arrTodo.length > 0
+							? arrTodo.length + " items left"
+							: ""}
+					</footer>
+				</div>
+			</div>
 		</div>
 	);
 };
